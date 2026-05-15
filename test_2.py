ad63a98dc68e70c988feba87851d87ae6b7a8c14
Markos Narinian <manarinian@gmail.com>
2026-05-15 10:43:15 +0300

    msg

--- test_2.py
+++ test_2.py
@@ -5,4 +5,10 @@ client = HeliusClient()
 response = client.get_signatures_for_address(
     "Ac4ytDhtmrWwA7d8tucsnKEWHo8SPjPKFNGGNA62YUXq"
 )
-pprint(response)
+for trasnaction_signature in response:
+    print(trasnaction_signature.signature)
+    print(trasnaction_signature.slot)
+    print(trasnaction_signature.err)
+    print(trasnaction_signature.memo)
+    print(trasnaction_signature.blockTime)
+    print(trasnaction_signature.confirmationStatus)
