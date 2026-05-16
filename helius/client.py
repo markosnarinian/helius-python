0db5831ebe45aa675c644da06c600cf800a71688
Markos Narinian <manarinian@gmail.com>
2026-05-16 21:11:06 +0300

    msg

--- helius/client.py
+++ helius/client.py
@@ -266,6 +266,19 @@ class HeliusClient:
         result = response.json()["result"]
         return result
 
+    def get_block_time(self, slot: int) -> int | None:
+        response = httpx.post(
+            f"https://mainnet.helius-rpc.com/?api-key={self.api_key}",
+            json={
+                "jsonrpc": "2.0",
+                "id": 1,
+                "method": "getBlockTime",
+                "params": [slot],
+            },
+        )
+        result = response.json()["result"]
+        return result
+
     @validate_call
     def get_signatures_for_address(
         self,
