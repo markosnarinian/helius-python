2b50a58612f3716c3bdac8c6656fdb8990c04da1
Markos Narinian <manarinian@gmail.com>
2026-05-30 15:31:11 +0300

    Arrange functions

--- src/helius/laserstream/websockets.py
+++ src/helius/laserstream/websockets.py
@@ -126,19 +126,52 @@ class WebSocketClient:
         response = self._send(request)
         return response["result"]
 
-    def logs_subscribe(
+    def transaction_subscribe(
         self,
         *,
-        filter: Literal["all", "allWithVotes"] | MentionsFilter,
+        vote: bool | None = None,
+        failed: bool | None = None,
+        signature: str | None = None,
+        account_include: list[str] | None = None,
+        account_exclude: list[str] | None = None,
+        account_required: list[str] | None = None,
         commitment: Literal["finalized", "confirmed", "processed"] | None = None,
-    ):
+        encoding: Literal["base58", "base64", "jsonParsed"] | None = None,
+        transaction_details: (
+            Literal["full", "signatures", "accounts", "none"] | None
+        ) = None,
+        show_rewards: bool | None = None,
+        max_supported_transaction_version: int | None = None,
+    ) -> int:
+        if max_supported_transaction_version is None and transaction_details in [
+            "accounts",
+            "full",
+        ]:
+            raise ValueError(
+                'max_supported_transaction_version is required when transaction_details is set to "accounts" or "full".'
+            )
+        filter = {
+            key: value
+            for key, value in {
+                "vote": vote,
+                "failed": failed,
+                "signature": signature,
+                "accountInclude": account_include,
+                "accountExclude": account_exclude,
+                "accountRequired": account_required,
+            }.items()
+            if value is not None
+        }
         request = (
-            JsonRpcRequest(method="logsSubscribe")
-            .add(filter)
+            JsonRpcRequest(method="transactionSubscribe")
+            .add(filter if filter else None)
             .set("commitment", commitment)
+            .set("encoding", encoding)
+            .set("transactionDetails", transaction_details)
+            .set("showRewards", show_rewards)
+            .set("maxSupportedTransactionVersion", max_supported_transaction_version)
             .build()
         )
-        self._send(request)
         response = self._send(request)
         subscription = response["result"]
         return subscription
@@ -189,6 +222,23 @@ class WebSocketClient:
         subscription = response["result"]
         return subscription
 
+    def logs_subscribe(
+        self,
+        *,
+        filter: Literal["all", "allWithVotes"] | MentionsFilter,
+        commitment: Literal["finalized", "confirmed", "processed"] | None = None,
+    ):
+        request = (
+            JsonRpcRequest(method="logsSubscribe")
+            .add(filter)
+            .set("commitment", commitment)
+            .build()
+        )
+        self._send(request)
+        response = self._send(request)
+        subscription = response["result"]
+        return subscription
+
     def program_subscribe(
         self,
         *,
@@ -212,56 +262,6 @@ class WebSocketClient:
 
     def root_subscribe(self) -> int:
         request = JsonRpcRequest(method="rootSubscribe").build()
-        response = self._send(request)
-        subscription = response["result"]
-        return subscription
-
-    def transaction_subscribe(
-        self,
-        *,
-        vote: bool | None = None,
-        failed: bool | None = None,
-        signature: str | None = None,
-        account_include: list[str] | None = None,
-        account_exclude: list[str] | None = None,
-        account_required: list[str] | None = None,
-        commitment: Literal["finalized", "confirmed", "processed"] | None = None,
-        encoding: Literal["base58", "base64", "jsonParsed"] | None = None,
-        transaction_details: (
-            Literal["full", "signatures", "accounts", "none"] | None
-        ) = None,
-        show_rewards: bool | None = None,
-        max_supported_transaction_version: int | None = None,
-    ) -> int:
-        if max_supported_transaction_version is None and transaction_details in [
-            "accounts",
-            "full",
-        ]:
-            raise ValueError(
-                'max_supported_transaction_version is required when transaction_details is set to "accounts" or "full".'
-            )
-        filter = {
-            key: value
-            for key, value in {
-                "vote": vote,
-                "failed": failed,
-                "signature": signature,
-                "accountInclude": account_include,
-                "accountExclude": account_exclude,
-                "accountRequired": account_required,
-            }.items()
-            if value is not None
-        }
-        request = (
-            JsonRpcRequest(method="transactionSubscribe")
-            .add(filter if filter else None)
-            .set("commitment", commitment)
-            .set("encoding", encoding)
-            .set("transactionDetails", transaction_details)
-            .set("showRewards", show_rewards)
-            .set("maxSupportedTransactionVersion", max_supported_transaction_version)
-            .build()
-        )
         response = self._send(request)
         subscription = response["result"]
         return subscription
